f7062323b122f0ab895622be674bdbb33c33b0c2
Enes ÇEB <enesceb@hotmail.com>
2022-10-11 16:40:15 +0300

    Inıtial Project
    Created Rabbitmq connection and config file, created exchanges, queues and bind queues

---
+++ [RabbitMQ .Net].Sample.Project/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace _RabbitMQ.Net_.Sample.Project
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConnectRabbitmq = new System.Windows.Forms.Button();
+            this.btnCreateExchange = new System.Windows.Forms.Button();
+            this.btnCreateQueues = new System.Windows.Forms.Button();
+            this.btnBindQueues = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnConnectRabbitmq
+            // 
+            this.btnConnectRabbitmq.Location = new System.Drawing.Point(12, 12);
+            this.btnConnectRabbitmq.Name = "btnConnectRabbitmq";
+            this.btnConnectRabbitmq.Size = new System.Drawing.Size(128, 35);
+            this.btnConnectRabbitmq.TabIndex = 0;
+            this.btnConnectRabbitmq.Text = "Connect RabbitMQ";
+            this.btnConnectRabbitmq.UseVisualStyleBackColor = true;
+            this.btnConnectRabbitmq.Click += new System.EventHandler(this.btnConnectRabbitmq_Click);
+            // 
+            // btnCreateExchange
+            // 
+            this.btnCreateExchange.Location = new System.Drawing.Point(12, 53);
+            this.btnCreateExchange.Name = "btnCreateExchange";
+            this.btnCreateExchange.Size = new System.Drawing.Size(128, 35);
+            this.btnCreateExchange.TabIndex = 1;
+            this.btnCreateExchange.Text = "Create Exchange\r\n";
+            this.btnCreateExchange.UseVisualStyleBackColor = true;
+            this.btnCreateExchange.Click += new System.EventHandler(this.btnCreateExchange_Click);
+            // 
+            // btnCreateQueues
+            // 
+            this.btnCreateQueues.Location = new System.Drawing.Point(12, 94);
+            this.btnCreateQueues.Name = "btnCreateQueues";
+            this.btnCreateQueues.Size = new System.Drawing.Size(128, 35);
+            this.btnCreateQueues.TabIndex = 2;
+            this.btnCreateQueues.Text = "Create Queues";
+            this.btnCreateQueues.UseVisualStyleBackColor = true;
+            this.btnCreateQueues.Click += new System.EventHandler(this.btnCreateQueues_Click);
+            // 
+            // btnBindQueues
+            // 
+            this.btnBindQueues.Location = new System.Drawing.Point(12, 135);
+            this.btnBindQueues.Name = "btnBindQueues";
+            this.btnBindQueues.Size = new System.Drawing.Size(128, 35);
+            this.btnBindQueues.TabIndex = 3;
+            this.btnBindQueues.Text = "Bind Queues";
+            this.btnBindQueues.UseVisualStyleBackColor = true;
+            this.btnBindQueues.Click += new System.EventHandler(this.btnBindQueues_Click);
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnBindQueues);
+            this.Controls.Add(this.btnCreateQueues);
+            this.Controls.Add(this.btnCreateExchange);
+            this.Controls.Add(this.btnConnectRabbitmq);
+            this.Name = "Form";
+            this.Text = " [RabbitMQ .Net] C#";
+            this.Load += new System.EventHandler(this.Form_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConnectRabbitmq;
+        private System.Windows.Forms.Button btnCreateExchange;
+        private System.Windows.Forms.Button btnCreateQueues;
+        private System.Windows.Forms.Button btnBindQueues;
+    }
+}
+
---
+++ [RabbitMQ .Net].Sample.Project/Form1.cs
@@ -0,0 +1,68 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Configuration;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _RabbitMQ.Net_.Sample.Project
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        private IConnection rabbitMqConnection;
+
+        public Form()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConnectRabbitmq_Click(object sender, EventArgs e)
+        {
+            var connection = ConfigurationManager.ConnectionStrings["RabbitMqConnection"].ConnectionString;
+            var connectionFactory  = new ConnectionFactory();
+            connectionFactory.Uri = new Uri(connection);
+            connectionFactory.AutomaticRecoveryEnabled = true;
+            connectionFactory.DispatchConsumersAsync = true;
+            rabbitMqConnection = connectionFactory.CreateConnection("DemoAppClient");
+
+        }
+
+        private void btnCreateExchange_Click(object sender, EventArgs e)
+        {
+            using (var channel = rabbitMqConnection.CreateModel())
+            {
+                channel.ExchangeDeclare("CustomerNotification", ExchangeType.Direct ,true, false );
+            } 
+        }
+
+        private void Form_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCreateQueues_Click(object sender, EventArgs e)
+        {
+            using (var queue = rabbitMqConnection.CreateModel())
+            {
+                queue.QueueDeclare("Email", true, false, false);
+                queue.QueueDeclare("Sms", true, false, false);
+
+            }
+        }
+
+        private void btnBindQueues_Click(object sender, EventArgs e)
+        {
+            using (var queuebind = rabbitMqConnection.CreateModel())
+            {
+                queuebind.QueueBind("Email", "CustomerNotification", "email");
+                queuebind.QueueBind("Sms", "CustomerNotification", "sms");
+
+            }
+        }
+    }
+}
