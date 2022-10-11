741bad67c55e41db94057db3a4caac8a77388ed9
Enes ÇEB <enesceb@hotmail.com>
2022-10-11 17:49:48 +0300

    Finalize

--- [RabbitMQ .Net].Sample.Project/Form1.Designer.cs
+++ [RabbitMQ .Net].Sample.Project/Form1.Designer.cs
@@ -48,9 +48,9 @@
             // 
             // btnConnectRabbitmq
             // 
-            this.btnConnectRabbitmq.Location = new System.Drawing.Point(12, 12);
+            this.btnConnectRabbitmq.Location = new System.Drawing.Point(12, 15);
             this.btnConnectRabbitmq.Name = "btnConnectRabbitmq";
-            this.btnConnectRabbitmq.Size = new System.Drawing.Size(128, 35);
+            this.btnConnectRabbitmq.Size = new System.Drawing.Size(170, 35);
             this.btnConnectRabbitmq.TabIndex = 0;
             this.btnConnectRabbitmq.Text = "Connect RabbitMQ";
             this.btnConnectRabbitmq.UseVisualStyleBackColor = true;
@@ -58,9 +58,9 @@
             // 
             // btnCreateExchange
             // 
-            this.btnCreateExchange.Location = new System.Drawing.Point(12, 53);
+            this.btnCreateExchange.Location = new System.Drawing.Point(12, 56);
             this.btnCreateExchange.Name = "btnCreateExchange";
-            this.btnCreateExchange.Size = new System.Drawing.Size(128, 35);
+            this.btnCreateExchange.Size = new System.Drawing.Size(170, 35);
             this.btnCreateExchange.TabIndex = 1;
             this.btnCreateExchange.Text = "Create Exchange\r\n";
             this.btnCreateExchange.UseVisualStyleBackColor = true;
@@ -68,9 +68,9 @@
             // 
             // btnCreateQueues
             // 
-            this.btnCreateQueues.Location = new System.Drawing.Point(12, 94);
+            this.btnCreateQueues.Location = new System.Drawing.Point(12, 97);
             this.btnCreateQueues.Name = "btnCreateQueues";
-            this.btnCreateQueues.Size = new System.Drawing.Size(128, 35);
+            this.btnCreateQueues.Size = new System.Drawing.Size(170, 35);
             this.btnCreateQueues.TabIndex = 2;
             this.btnCreateQueues.Text = "Create Queues";
             this.btnCreateQueues.UseVisualStyleBackColor = true;
@@ -78,9 +78,9 @@
             // 
             // btnBindQueues
             // 
-            this.btnBindQueues.Location = new System.Drawing.Point(12, 135);
+            this.btnBindQueues.Location = new System.Drawing.Point(12, 138);
             this.btnBindQueues.Name = "btnBindQueues";
-            this.btnBindQueues.Size = new System.Drawing.Size(128, 35);
+            this.btnBindQueues.Size = new System.Drawing.Size(170, 35);
             this.btnBindQueues.TabIndex = 3;
             this.btnBindQueues.Text = "Bind Queues";
             this.btnBindQueues.UseVisualStyleBackColor = true;
@@ -88,16 +88,16 @@
             // 
             // txtPublishEmail
             // 
-            this.txtPublishEmail.Location = new System.Drawing.Point(12, 257);
+            this.txtPublishEmail.Location = new System.Drawing.Point(16, 226);
             this.txtPublishEmail.Name = "txtPublishEmail";
-            this.txtPublishEmail.Size = new System.Drawing.Size(265, 20);
+            this.txtPublishEmail.Size = new System.Drawing.Size(166, 20);
             this.txtPublishEmail.TabIndex = 4;
             // 
             // btnPublishEmail
             // 
-            this.btnPublishEmail.Location = new System.Drawing.Point(150, 283);
+            this.btnPublishEmail.Location = new System.Drawing.Point(66, 252);
             this.btnPublishEmail.Name = "btnPublishEmail";
-            this.btnPublishEmail.Size = new System.Drawing.Size(127, 31);
+            this.btnPublishEmail.Size = new System.Drawing.Size(116, 31);
             this.btnPublishEmail.TabIndex = 5;
             this.btnPublishEmail.Text = "Publish Email";
             this.btnPublishEmail.UseVisualStyleBackColor = true;
@@ -106,7 +106,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 238);
+            this.label1.Location = new System.Drawing.Point(17, 207);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(72, 13);
             this.label1.TabIndex = 6;
@@ -115,7 +115,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(17, 330);
+            this.label2.Location = new System.Drawing.Point(21, 299);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(67, 13);
             this.label2.TabIndex = 9;
@@ -123,9 +123,9 @@
             // 
             // btnPublishSms
             // 
-            this.btnPublishSms.Location = new System.Drawing.Point(154, 375);
+            this.btnPublishSms.Location = new System.Drawing.Point(70, 344);
             this.btnPublishSms.Name = "btnPublishSms";
-            this.btnPublishSms.Size = new System.Drawing.Size(127, 31);
+            this.btnPublishSms.Size = new System.Drawing.Size(116, 31);
             this.btnPublishSms.TabIndex = 8;
             this.btnPublishSms.Text = "Publish SMS";
             this.btnPublishSms.UseVisualStyleBackColor = true;
@@ -133,15 +133,15 @@
             // 
             // txtPublishSms
             // 
-            this.txtPublishSms.Location = new System.Drawing.Point(16, 349);
+            this.txtPublishSms.Location = new System.Drawing.Point(20, 318);
             this.txtPublishSms.Name = "txtPublishSms";
-            this.txtPublishSms.Size = new System.Drawing.Size(265, 20);
+            this.txtPublishSms.Size = new System.Drawing.Size(166, 20);
             this.txtPublishSms.TabIndex = 7;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(370, 15);
+            this.label3.Location = new System.Drawing.Point(201, 16);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(83, 13);
             this.label3.TabIndex = 10;
@@ -150,7 +150,7 @@
             // lstEmailMessages
             // 
             this.lstEmailMessages.FormattingEnabled = true;
-            this.lstEmailMessages.Location = new System.Drawing.Point(374, 35);
+            this.lstEmailMessages.Location = new System.Drawing.Point(205, 36);
             this.lstEmailMessages.Name = "lstEmailMessages";
             this.lstEmailMessages.Size = new System.Drawing.Size(392, 134);
             this.lstEmailMessages.TabIndex = 11;
@@ -158,7 +158,7 @@
             // lstSmsMessages
             // 
             this.lstSmsMessages.FormattingEnabled = true;
-            this.lstSmsMessages.Location = new System.Drawing.Point(374, 203);
+            this.lstSmsMessages.Location = new System.Drawing.Point(205, 203);
             this.lstSmsMessages.Name = "lstSmsMessages";
             this.lstSmsMessages.Size = new System.Drawing.Size(392, 134);
             this.lstSmsMessages.TabIndex = 13;
@@ -166,7 +166,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(370, 183);
+            this.label4.Location = new System.Drawing.Point(201, 182);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(81, 13);
             this.label4.TabIndex = 12;
@@ -174,7 +174,7 @@
             // 
             // btnSubscribeEmailQueue
             // 
-            this.btnSubscribeEmailQueue.Location = new System.Drawing.Point(374, 343);
+            this.btnSubscribeEmailQueue.Location = new System.Drawing.Point(309, 344);
             this.btnSubscribeEmailQueue.Name = "btnSubscribeEmailQueue";
             this.btnSubscribeEmailQueue.Size = new System.Drawing.Size(139, 31);
             this.btnSubscribeEmailQueue.TabIndex = 14;
@@ -184,7 +184,7 @@
             // 
             // btnSubscribeSmsQueue
             // 
-            this.btnSubscribeSmsQueue.Location = new System.Drawing.Point(524, 343);
+            this.btnSubscribeSmsQueue.Location = new System.Drawing.Point(459, 344);
             this.btnSubscribeSmsQueue.Name = "btnSubscribeSmsQueue";
             this.btnSubscribeSmsQueue.Size = new System.Drawing.Size(139, 31);
             this.btnSubscribeSmsQueue.TabIndex = 15;
@@ -196,7 +196,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(618, 396);
             this.Controls.Add(this.btnSubscribeSmsQueue);
             this.Controls.Add(this.btnSubscribeEmailQueue);
             this.Controls.Add(this.lstSmsMessages);
